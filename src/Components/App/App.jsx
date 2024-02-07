6b5dfe18201af7be2d51415ceaa74a172723490f
Igor Piliaiev <igorpilaev27@gmail.com>
2024-02-07 18:43:25 +0200

    +

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,8 +1,9 @@
-import "./App.css";
-import { Profile } from "../Profile/Profile";
 import { FriendList } from "../FriendList/FriendList";
-import userData from "../userData.json";
-import friends from "../friends.json";
+import { Profile } from "../Profile/Profile";
+import userData from "../../userData.json";
+import friends from "../../friends.json";
+
+import "./App.css";
 
 function App() {
   return (
